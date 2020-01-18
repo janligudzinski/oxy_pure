ebc66a8a5607ea296b8b45e5ec96b219f2e992ef
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-18 18:24:03 +0100

    the purifier now runs in an endless thread

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 #[macro_use]
 extern crate log;
 
+use std::sync::Mutex;
+use std::thread;
 use crate::core::Purifier;
 use pretty_env_logger::init;
+use std::time::Duration;
+
 mod core;
 type ImapError = imap::error::Error;
 
 fn main() {
     init();
-    let mut purifier = Purifier::new();
-    if let Err(e) = purifier.run() {
-           error!("{:?}", e);
-    }
+    let mut purifier = Mutex::new(Purifier::new());
+    thread::spawn(move || {
+        loop {
+            {
+                let purifier = purifier.get_mut().unwrap();
+                if let Err(e) = purifier.run() {
+                    println!("{}", e);
+                }
+            }
+            thread::sleep(Duration::from_millis(2000));
+        }
+    }).join().unwrap();
 }
