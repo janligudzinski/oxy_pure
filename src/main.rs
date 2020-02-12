bb0e8cfa55d6542632937b5ecadac16f9720f5e4
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-02-12 17:39:24 +0100

    purged useless type

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,6 @@ use crate::core::Purifier;
 
 type ImapError = imap::error::Error;
 
-#[derive(Debug)]
-struct MyError {}
-impl std::fmt::Display for MyError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), ::core::fmt::Error> {
-        write!(f, "placeholder type to satisfy Iron")?;
-        Ok(())
-    }
-}
-impl std::error::Error for MyError {}
-
 fn main() {
     init();
     let mut purifier = Purifier::new();
