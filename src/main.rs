167290fba68d71ee151359c5725e67d8ab994e68
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-02-12 17:44:33 +0100

    lambda-fied the program

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 #[macro_use]
 extern crate log;
 
+use aws_lambda_events::event::cloudwatch_events::CloudWatchEvent;
+use lambda_runtime::{error::HandlerError, lambda};
 use pretty_env_logger::init;
+use std::error::Error;
 use std::fmt::Formatter;
-
 mod core;
 use crate::core::Purifier;
-
+use serde::Serialize;
 type ImapError = imap::error::Error;
 
-fn main() {
-    init();
+#[derive(Serialize)]
+pub enum Output {
+    Success,
+    Failure,
+}
+// The program will be triggered by AWS CloudWatch timer events on a schedule.
+fn handler(_ev: CloudWatchEvent, _c: lambda_runtime::Context) -> Result<Output, HandlerError> {
     let mut purifier = Purifier::new();
     match purifier.run() {
-        Ok(_) => (),
-        Err(e) => error!("{}", e),
+        Ok(_) => Ok(Output::Success),
+        _ => Ok(Output::Failure),
     }
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    init();
+    lambda!(handler);
+    Ok(())
 }
